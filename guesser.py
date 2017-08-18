44e11103849d92f9f68755740f87237dbb235cb3
Devin Carter <the.devin.carter@outlook.com>
2017-08-18 16:07:29 -0500

    Completed game

--- guesser.py
+++ guesser.py
@@ -7,19 +7,27 @@ import random
 # define main function
 def main():
     print("Guess a number between 1 and 100")
-    randomNumber = 35
-    # set randomNumber value to string
-    randomNumber = str(randomNumber)
+    # randomNumber = 35 # for debugging only
+    randomNumber = random.randint(1, 100)
 
     found = False  # flag variable to see if a correct guess has been made.
 
+    # Run through the guessing program
     while not found:
-        userGuess = input("Your Guess: ")
+        userGuess = int(input("Your Guess: ")) # turn str that is user guess into integer
         if userGuess == randomNumber:
             print("You got it right!")
             found = True
+        elif userGuess > randomNumber:
+            print("Guess a lower number!")
         else:
-            print("Sorry, try again.")
+            print("Guess a higher number!")
+
+    # Statement when game is won
+    print("Thanks for playing!")
+
+
+
 
 
 main()
